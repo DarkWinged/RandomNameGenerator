049ccf8cd0baf8540a1444841e185ae8f0a934c5
crazyzoo143 <devcrazyzoo143@gmail.com>
2023-08-28 17:50:49 +0000

    load data

--- rng_app.py
+++ rng_app.py
@@ -2,12 +2,13 @@
 #james rogers|james.levi.rogers@gmail.com
 
 from flask import Flask, redirect, render_template, request
+import yaml
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', ancestries=['Human','Elf','Dwarf']) # replace the static list with a list of the keys in app.config[<data_variable_name>]
+    return render_template('index.html', ancestries=list(app.config['ancestry_generate'].keys()))
 
 @app.route('/names', methods=['POST'])
 def names():
@@ -17,7 +18,9 @@ def names():
     return 'To do: make a names.html and names.css'
 
 if __name__ == '__main__':
-    #load in the data from the yaml
-    #save the data in app.config[<data_variable_name>]
+    with open('racegendernames.yaml', 'r') as yaml_file:
+        ancestor_generate = yaml.safe_load(yaml_file)
+    app.config['ancestry_generate'] = ancestor_generate
+    
     app.run(host='0.0.0.0', port='2224', debug=True)
 
