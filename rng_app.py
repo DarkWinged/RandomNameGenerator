690c759d7098815f4b2ae2c163be0a37785484c2
dbeerhorst <student@bchd.12a645f5-5f96-41e8-9ab9-46d4a0d350a2>
2023-08-28 17:22:45 +0000

    Updated rng_app.py

--- rng_app.py
+++ rng_app.py
@@ -1,21 +1,34 @@
 #! /usr/bin/env python3
-#james rogers|james.levi.rogers@gmail.com
+# james rogers | james.levi.rogers@gmail.com
 
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', ancestries=['Human','Elf','Dwarf'])
+    return render_template('index.html', ancestries=['Human', 'Elf', 'Dwarf'])
 
 @app.route('/names', methods=['POST'])
 def names():
     ancestry = request.form.get('ancestry', 'Human').capitalize()
     gender = request.form.get('gender', 'male').lower()
-    count = int(request.form.get('count', '5'))
+    count_str = request.form.get('count', '5')
+
+    if not count_str.isdigit():
+        return redirect(url_for('index'))
+
+    count = int(count_str)
+    if count <= 0 or count > 50:
+        return redirect(url_for('index'))
+
+    if ancestry not in ['Human', 'Elf', 'Dwarf']: return redirect(url_for('index'))
+    if gender not in ['male', 'female']: return redirect(url_for('index'))
+
+    # Here you'll handle the name generation based on the input criteria (ancestry, gender, count)
+    # For now, I'm returning a placeholder string
     return 'To do: make a names.html and names.css'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='2224', debug=True)
+    app.run(host='0.0.0.0', port=2224, debug=True)
 
