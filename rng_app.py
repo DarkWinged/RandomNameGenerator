f511b57a8f65760660ce7bf29dd19758df95fc86
DarkWinged <james.levi.rogers@gmail.com>
2023-08-28 15:44:17 +0000

    added instruction comments

--- rng_app.py
+++ rng_app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', ancestries=['Human','Elf','Dwarf'])
+    return render_template('index.html', ancestries=['Human','Elf','Dwarf']) # replace the static list with a list of the keys in app.config[<data_variable_name>]
 
 @app.route('/names', methods=['POST'])
 def names():
@@ -17,5 +17,7 @@ def names():
     return 'To do: make a names.html and names.css'
 
 if __name__ == '__main__':
+    #load in the data from the yaml
+    #save the data in app.config[<data_variable_name>]
     app.run(host='0.0.0.0', port='2224', debug=True)
 
